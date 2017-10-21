0c210771ed431f9a00cc2cab3932722a4e65a283
ubuser <canada.samchen0904@gmail.com>
2017-10-20 20:49:15 -0500

    6th commit

---
+++ ubuser/Shop_cashier.py
@@ -0,0 +1,10 @@
+wallet = input('How much do you have?: ')
+money = int(wallet)
+
+
+items = {'apple': 2, 'banana': 4, 'orange': 6}
+
+for fruit in items:
+    print('--------------------------------------')
+    print('You have $' + wallet + ' in your wallet.')
+    print('Each ' + fruit + ' costs ' + )
---
+++ ubuser/test.py
@@ -0,0 +1,17 @@
+gh = ['enfavsoerganjcvfor']
+#print(dir(gh))
+
+print('----------------------------------------------------------------------------------')
+
+cd = 'wfvjnkqwe'
+#print(dir(cd))
+
+#print(gh.upper())
+
+rec = ('~/Documents/mbox.txt')
+fh = open(rec)
+for line in fh:
+    print(line)
+
+
+
